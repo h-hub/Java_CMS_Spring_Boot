7162f2657566920ef597bd20fe5488ae77312938
h-hub <harsha.kj89@gmail.com>
2018-11-06 01:22:36 +0800

    front end code clean up and refactoring

---
+++ src/main/resources/static/js/blog-summernote.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+
+	$('#content').summernote({
+			height : 500, // set editor height
+			minHeight : null, // set minimum height of editor
+			maxHeight : null // set maximum height of editor
+		});
+
+});
--- src/main/resources/static/js/blog.js
+++ src/main/resources/static/js/blog.js
@@ -11,12 +11,6 @@ $(document).ready(function() {
 		startDate : new Date()
 	});
 
-	$('#content').summernote({
-		height : 500, // set editor height
-		minHeight : null, // set minimum height of editor
-		maxHeight : null // set maximum height of editor
-	});
-
 	$('#preview-btn').click(function(event) {
 
 		event.preventDefault();
